40f02fb6b4b7f8d8d648d524fbed87e615e0f9f7
Matt Hink <matt.hink@formidable.com>
2016-09-12 11:23:55 -0700

    Change module include to placate my consumer project...

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { h, render } from "preact";
 
-import VictoryLineChart from "components/victory-line-chart";
+import VictoryLineChart from "./components/victory-line-chart.jsx";
 
 export default class VictoryStandalone {
   constructor(dom, props) {
